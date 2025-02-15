224b299befad98dd8dc53b4c49ecc4ebfb4431fb
PaulNero <58290662+PaulNero@users.noreply.github.com>
2025-02-15 23:25:39 +0300

    new version bot

--- app.py
+++ app.py
@@ -8,29 +8,37 @@ from aiogram.fsm.context import FSMContext
 from aiogram.fsm.state import State, StatesGroup
 from aiogram.filters import Command
 from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
-import data
-import re
+import secret
 
-TOKEN = data.TOKEN
-CHANNEL_ID = data.CHANNEL_ID
+TOKEN = secret.TOKEN
+CHANNEL_ID = secret.CHANNEL_ID
 CSV_FILE = "REBEL_BIBLE_BOT.csv"
+LINK = secret.LINK
 
-# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 
-# Инициализация бота и диспетчера
 bot = Bot(token=TOKEN)
 storage = MemoryStorage()
-dp = Dispatcher()
+dp = Dispatcher(storage=storage)
 
-# Определение состояний
 class Survey(StatesGroup):
+    name_city = State()
     price = State()
     brands = State()
-    form = State()
+    contact = State()
 
+def get_brands_keyboard(selected_brands):
+    keyboard = []
+    brands = ["Reuzel", "Lock Stock", "Morgan’s", "REBEL", "NishMan", "Dream Catcher", "Boy's Toys", "KONDOR",
+              "White Cosmetics", "Maestro"]
+    for brand in brands:
+        text = f"✅ {brand}" if brand in selected_brands else brand
+        keyboard.append([InlineKeyboardButton(text=text, callback_data=f"brand_{brand}")])
+    if selected_brands:
+        keyboard.append([InlineKeyboardButton(text="❌ Отменить выбор", callback_data="cancel_brand")])
+    keyboard.append([InlineKeyboardButton(text="✅ Завершить выбор", callback_data="finish_brands")])
+    return InlineKeyboardMarkup(inline_keyboard=keyboard)
 
-# Клавиатура для вопроса о стоимости стрижки
 price_keyboard = ReplyKeyboardMarkup(
     keyboard=[
         [KeyboardButton(text="Дешевле 800 руб."), KeyboardButton(text="800–1000 руб.")],
@@ -41,27 +49,18 @@ price_keyboard = ReplyKeyboardMarkup(
     one_time_keyboard=True
 )
 
-
-# Функция для обновления клавиатуры с брендами
-def get_brands_keyboard(selected_brands):
-    keyboard = []
-    brands = ["Reuzel", "Lock Stock", "Morgan’s", "REBEL", "NishMan", "Dream Catcher", "Boy's Toys", "KONDOR",
-              "White Cosmetics", "Maestro"]
-    for brand in brands:
-        text = f"✅ {brand}" if brand in selected_brands else brand
-        keyboard.append([InlineKeyboardButton(text=text, callback_data=f"brand_{brand}")])
-    if selected_brands:
-        keyboard.append([InlineKeyboardButton(text="❌ Отменить выбор", callback_data="cancel_brand")])
-        keyboard.append([InlineKeyboardButton(text="✅ Завершить выбор", callback_data="finish_brands")])
-    return InlineKeyboardMarkup(inline_keyboard=keyboard)
-
-
 @dp.message(Command("start"))
 async def start_command(message: types.Message, state: FSMContext):
-    await message.answer("Привет! Чтобы получить бесплатный гайд, пройди опрос. Это займет 20 секунд")
-    await message.answer("Стоимость стрижки по твоему прайсу?", reply_markup=price_keyboard)
-    await state.set_state(Survey.price)
+    await message.answer(
+        "Привет! Для начала давай познакомимся) Напиши свое имя и в каком городе работаешь и живешь?🙂")
+    await state.set_state(Survey.name_city)
 
+@dp.message(Survey.name_city)
+async def process_name_city(message: types.Message, state: FSMContext):
+    await state.update_data(name_city=message.text, username=message.from_user.username)
+    await message.answer("Кайф! Очень приятно🤗 Сколько сейчас по твоему прайсу стоит услуга «мужская стрижка»?",
+                         reply_markup=price_keyboard)
+    await state.set_state(Survey.price)
 
 @dp.message(Survey.price)
 async def process_price(message: types.Message, state: FSMContext):
@@ -70,7 +69,7 @@ async def process_price(message: types.Message, state: FSMContext):
         await message.answer("Пожалуйста, выбери вариант из списка.")
         return
     await state.update_data(price=message.text)
-    await message.answer("На каких брендах косметики работаешь? (Выбери из списка)",
+    await message.answer("Норм-норм)) А на каких брендах косметики работаешь? (можно выбрать несколько вариантов)",
                          reply_markup=get_brands_keyboard([]))
     await state.set_state(Survey.brands)
 
@@ -82,86 +81,57 @@ async def process_brand(callback: types.CallbackQuery, state: FSMContext):
 
     if callback.data.startswith("brand_"):
         brand = callback.data.replace("brand_", "")
-        if brand in selected_brands:
-            selected_brands.remove(brand)  # Удаляет конкретный бренд из списка
-        else:
-            selected_brands.append(brand)  # Добавляет бренд, если он не был выбран
+        if brand not in selected_brands:
+            selected_brands.append(brand)
         await state.update_data(brands=selected_brands)
-
     elif callback.data == "cancel_brand" and selected_brands:
         selected_brands.pop()
         await state.update_data(brands=selected_brands)
-
     elif callback.data == "finish_brands":
-        await callback.message.delete_reply_markup()  # Скрытие клавиатуры
-        await callback.message.answer("Гайд почти твой! Осталось заполнить небольшую анкету.")
-        await callback.message.answer("Введите ФИО: ")
-        await state.set_state(Survey.form)
+        await callback.message.delete_reply_markup()
+        await callback.message.answer(
+            "Крутяк!👏 Остался последний шаг и гайд твой! Напиши ник своего аккаунта в инстаграм (пример: @ahuennyi_barber) и номер телефона.")
+        await state.set_state(Survey.contact)
         return
-
     await callback.message.edit_reply_markup(reply_markup=get_brands_keyboard(selected_brands))
     await callback.answer()
 
-
-@dp.message(Survey.form)
-async def process_form(message: types.Message, state: FSMContext):
+@dp.message(Survey.contact)
+async def process_contact(message: types.Message, state: FSMContext):
+    await state.update_data(contact=message.text)
     data = await state.get_data()
-    form_data = data.get("form", [])
-    form_data.append(message.text)
-    await state.update_data(form=form_data)
-    if len(form_data) == 1:
-        await message.answer("Введите номер телефона:")
 
-    elif len(form_data) == 2:
-        phone_pattern = re.compile(r"^\+\d{10,15}$")
-        if not phone_pattern.match(form_data[1]):
-            await message.answer("Пожалуйста, введите корректный номер телефона в формате +79111234567")
-            form_data.pop()
-            return
-        await message.answer("Введите ваш аккаунт в Instagram:")
+    file_exists = os.path.isfile(CSV_FILE)
+    with open(CSV_FILE, "a", newline="", encoding="utf-8") as file:
+        writer = csv.writer(file)
+        if not file_exists:
+            writer.writerow(["№", "Имя и Город", "Username", "Цена", "Бренды", "Контакты"])
+        row_num = sum(1 for _ in open(CSV_FILE)) if file_exists else 1
+        row_num = max(row_num, 0)
+        writer.writerow(
+            [row_num, data.get("name_city"), data.get("username"), data.get("price"), ", ".join(data.get("brands", [])),
+             data.get("contact")])
 
-    elif len(form_data) == 3:
-        await message.answer("Введите ваш город:")
+    # Отправка Анкеты в канал
+    survey_text = (f"📋 Новая анкета!\n\n"
+                   f"👤 Имя и город: {data.get('name_city')}\n"
+                   f"🔗 Username: @{data.get('username')}\n"
+                   f"💰 Цена: {data.get('price')}\n"
+                   f"💄 Бренды: {', '.join(data.get('brands', []))}\n"
+                   f"📞 Контакты: {data.get('contact')}")
+    await bot.send_message(CHANNEL_ID, survey_text)
 
-    else:
-        telegram_id = message.from_user.id
-        telegram_username = message.from_user.username or "Не указан"
-        await state.update_data(contact={
-            "ФИО": form_data[0],
-            "Телефон": form_data[1],
-            "Instagram": form_data[2],
-            "Город": form_data[3],
-            "Telegram ID": telegram_id,
-            "Telegram Username": telegram_username
-        })
-        await message.answer(f"Спасибо! Вот ссылка на гайд: {data.LINK}")
-        file_exists = os.path.isfile(CSV_FILE)
-        with open(CSV_FILE, mode='a', newline='') as file:
-            writer = csv.writer(file)
-            if not file_exists:
-                writer.writerow(["№", "Ценовой диапазон", "Бренды", "ФИО", "Номер телефона", "Instagram", "Город", "Telegram ID",
-                                 "Telegram Username"])
-            row_num = sum(1 for _ in open(CSV_FILE)) if file_exists else 0
-            writer.writerow(
-                [row_num, data['price'], data['brands'], form_data[0], form_data[1], form_data[2], form_data[3], telegram_id,
-                 telegram_username])
-        await bot.send_document(CHANNEL_ID, types.FSInputFile(CSV_FILE))
-        message_text = (f"📌 *Новая заявка!*\n\n"
-                        f"*Ценовой диапазон:* {data['price']}\n"
-                        f"*Бренды:* {data['brands']}\n"
-                        f"*ФИО:* {form_data[0]}\n"
-                        f"*Номер телефона:* {form_data[1]}\n"
-                        f"*Ссылка на Instagram:* https://instagram.com/{form_data[2]}\n"
-                        f"*Город:* {form_data[3]}\n"
-                        f"*Telegram ID:* {telegram_id}\n"
-                        f"*Telegram Username:* @{telegram_username}")
-        await bot.send_message(CHANNEL_ID, message_text, parse_mode="Markdown")
-        await state.clear()
+    # Отправка CSV-файла в канал
+    await bot.send_document(CHANNEL_ID, types.FSInputFile(CSV_FILE))
 
+    await message.answer(
+        f"Респект! Спасибо за твое время и интерес, забирай гайд! {LINK} \nИ да, знай, что я жду, когда о тебе узнают ВСЕ!))")
+    await state.clear()
 
 async def main():
-    await bot.delete_webhook(drop_pending_updates=True)
+    await bot.delete_webhook(drop_pending_updates=False)
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
